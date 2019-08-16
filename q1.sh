863a7c3444b9a183489be68811efd1db07b54e83
saumya-sharma <prasaman.ghosh@niser.ac.in>
2019-08-16 11:25:19 +0530

    Updated

---
+++ q1.sh
@@ -0,0 +1,5 @@
+echo "Your name please"
+read name
+mkdir $name
+cd $name
+touch $name.cpp
---
+++ q2.sh
@@ -0,0 +1,11 @@
+echo "Tell your name."
+read nm
+mkdir $nm
+cd $nm
+touch ${nm}1.cpp
+touch ${nm}2.cpp
+touch ${nm}3.cpp
+touch ${nm}4.cpp
+touch ${nm}5.cpp
+
+
